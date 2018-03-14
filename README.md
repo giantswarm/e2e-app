f712dc017dd7cecdab2b7eb9953bb0f2195aa999
Tim Schindler <xh3b4sd@gmail.com>
2018-03-14 17:52:55 +0100

    adds initial HTTP server (#1)

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+[![CircleCI](https://circleci.com/gh/giantswarm/e2e-app.svg?&style=shield&circle-token=4a9231c3fd0adf22193a186149ccaeb3a72188d4)](https://circleci.com/gh/giantswarm/e2e-app)
+
 # e2e-app
-Application run for integration tests within a e2e test environment on Kubernetes. 
+Application run for integration tests within a e2e test environment on Kubernetes.
---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/giantswarm/micrologger"
+	yaml "gopkg.in/yaml.v2"
+)
+
+const (
+	Port = "8080"
+)
+
+var (
+	description = "Application run for integration tests within a e2e test environment on Kubernetes."
+	gitCommit   = "n/a"
+	name        = "e2e-app"
+	source      = "https://github.com/giantswarm/e2e-app"
+)
+
+func main() {
+	if (len(os.Args) > 1) && (os.Args[1] == "version") {
+		d, err := yaml.Marshal(newVersionResponse())
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%s", d)
+
+		return
+	}
+
+	ctx := context.Background()
+	mux := http.NewServeMux()
+	logger, err := micrologger.New(micrologger.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	mux.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(newVersionResponse())
+	}))
+
+	mux.Handle("/delay/1", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(1 * time.Second)
+		io.WriteString(w, "Hello World")
+	}))
+	mux.Handle("/delay/5", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(5 * time.Second)
+		io.WriteString(w, "Hello World")
+	}))
+	mux.Handle("/delay/10", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(10 * time.Second)
+		io.WriteString(w, "Hello World")
+	}))
+
+	server := &http.Server{
+		Addr:    ":" + Port,
+		Handler: mux,
+	}
+
+	go func() {
+		logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("running server at http://0.0.0.0:%s", Port))
+		err := server.ListenAndServe()
+		if IsServerClosed(err) {
+			// fall through
+		} else if err != nil {
+			panic(err)
+		}
+	}()
+
+	stopChan := make(chan os.Signal)
+	signal.Notify(stopChan, syscall.SIGINT, syscall.SIGTERM)
+	<-stopChan
+
+	logger.LogCtx(ctx, "level", "debug", "message", "received termination signal")
+	logger.LogCtx(ctx, "level", "debug", "message", "draining server connections")
+
+	ctx, _ = context.WithTimeout(ctx, 5*time.Second)
+	server.Shutdown(ctx)
+
+	logger.LogCtx(ctx, "level", "debug", "message", "shutting down")
+}
